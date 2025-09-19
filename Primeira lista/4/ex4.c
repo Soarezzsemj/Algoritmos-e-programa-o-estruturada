164b423b3e6af4eee5305a0990b7f09708df3260
Soarezzsemj <popcornor6topster@outlook.com>
2025-09-19 13:17:58 -0300

    +1-1

--- Primeira lista/4/ex4.c
+++ Primeira lista/4/ex4.c
@@ -1,7 +1,20 @@
 // Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário, sabendo-se que este sofreu um aumento de 25%.
+#include <stdio.h>
 
 int main (void){
 
+        double salario;
+        double aumento; 
+        double salario_novo;
+
+    printf("digite aqui seu salario\n");
+    scanf("%lf", &salario);
+
+    aumento = (salario * 0.25);
+
+    salario_novo = ( aumento + salario);
+
+    printf("Seu salario aumentou para: %.2f\n", salario_novo);
 
     return 0;
     
