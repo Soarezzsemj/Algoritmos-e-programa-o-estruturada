e0d42746e4d0b2aab44d219f3afe9787e1124590
Soarezzsemj <popcornor6topster@outlook.com>
2025-09-30 12:41:21 -0300

    aula malvezzi

--- 4-Quarta lista/4/ex4.c
+++ 4-Quarta lista/4/ex4.c
@@ -2,3 +2,29 @@
 // convertê-lo para horas, minutos e segundos. Os segundos devem ser passados como parâmetro.
 // (4)
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void Calculo(int sec){
+    
+    int hora,min,aux;
+    hora = sec/3600;
+    min = sec%3600; // é o resto 
+    aux = min;
+    min = min / 60;
+    sec = aux % 60;
+
+    printf("horas:%d Minutos:%d Segundos:%d", hora, min, sec);
+
+}
+
+int main(){
+
+    int sec;
+    printf("digite os segundos");
+    scanf("%d", &sec);
+    Calculo(sec);
+    
+    return 0;
+}
+
