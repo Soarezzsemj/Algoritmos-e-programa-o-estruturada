eec232aca7f2ff1219f163b1745b2ccfcd199223
Soarezzsemj <popcornor6topster@outlook.com>
2025-10-10 22:43:34 -0300

    El erro

--- 5-Quinta Lista/1/ex1.c
+++ 5-Quinta Lista/1/ex1.c
@@ -1,5 +1,30 @@
-// Faça um programa que preencha um vetor com nove números inteiros, calcule e mostre
-// os números primos e suas respectivas posições
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
-#include <Stdio.h>
-#include <Stdlib.h>
+bool ehPrimo(int n) {
+ if (n <= 1) return false;
+ for (int i = 2; i <= sqrt(n); i++) {
+  if (n % i == 0)
+   return false;
+ }
+ return true;
+}
+
+int main() {
+ int v[9];
+
+ printf("Digite 9 números:\n");
+ for (int i = 0; i < 9; i++) {
+  scanf("%d", &v[i]);
+ }
+
+ printf("Números primos e suas posições:\n");
+ for (int i = 0; i < 9; i++) {
+  if (ehPrimo(v[i])) {
+   printf("Valor: %d | Posição: %d\n", v[i], i);
+  }
+ }
+
+ return 0;
+}
--- 5-Quinta Lista/2/ex2.c
+++ 5-Quinta Lista/2/ex2.c
@@ -1,6 +1,60 @@
-// Preencha dois vetores de 5 posições cada, e faça a troca de valores entre os vetores, de modo
-// que o primeiro vetor receba os valores do segundo vetor e o segundo vetor, receba os valores do
-//     primeiro vetor. Exiba os vetores antes e após a troca.
+//Preencha dois vetores de 5 posições cada, e faça a troca de valores entre os vetores, de modo
+//que o primeiro vetor receba os valores do segundo vetor e o segundo vetor, receba os valores do
+//primeiro vetor. Exiba os vetores antes e após a troca.
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int main () {
+
+  int v1 [5];
+  int v2 [5];
+    int v3 [5];
+
+
+    printf("digite aqui os valores do primeiro vetor");
+    for (int i =0; i < 5; i++) {
+        scanf("%d", &v1[i]);
+    }
+
+    printf("digite aqui os valores do segundo vetor");
+    for (int i =0; i < 5; i++) {
+        scanf("%d", &v2[i]);
+    }
+
+
+
+    printf("VALORES ANTES DA TROCA");
+    for (int i =0; i < 5; i++) {
+        printf(" \n VALOR DO VETOR 1 :Numero: %d | Posição: %d\n", v1[i], i);
+
+    }
+
+    for (int i =0; i < 5; i++) {
+
+        printf(" \n VALOR DO VETOR 2 :Numero: %d | Posição: %d\n", v2[i], i);
+    }
+
+
+     for ( int i = 0; i < 5; i++) {
+         v3[i] = v1[i];
+     }
+
+    printf("VALORES DEPOIS DA TROCA");
+
+    for ( int i = 0; i < 5; i++) {
+         v1[i] = v2[i];
+
+         printf(" \n VALOR DO VETOR 1 :Numero: %d | Posição: %d\n", v1[i], i);
+     }
+
+     for ( int i = 0; i < 5; i++) {
+         v2[i] = v3[i];
+
+        printf(" \n VALOR DO VETOR 2 :Numero: %d | Posição: %d\n", v2[i], i);
+     }
+
+
+    return 0;
+}
+
