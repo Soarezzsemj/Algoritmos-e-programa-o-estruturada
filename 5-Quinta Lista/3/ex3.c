73d6ab79b452dd020bfdcf38c8a7677dc43d3e25
Soarezzsemj <popcornor6topster@outlook.com>
2025-10-13 20:32:16 -0300

    ex3 feito

--- 5-Quinta Lista/3/ex3.c
+++ 5-Quinta Lista/3/ex3.c
@@ -15,25 +15,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void vetores(){
-
-    int vetorprecovenda[10];
-    int vetoruquantidadevendida[10];
-
-
-
-}
-
-
-
-
 
 int main() {
     double salario = 545;
     double acrescimo = 0.5;
     double valortotal;
 
-
+    int vetorprecovenda[10];
+    float vetorprecovenda[10];
+    int vetoruquantidadevendida[10];
 
 
     for (int i  = 0; i < 10; i++) {
@@ -41,10 +31,32 @@ int main() {
     scanf("%i", &vetorprecovenda[i]);
         printf("\n Digite a quantidade vendida: ");
         scanf("%i", &vetoruquantidadevendida[i]);
+    float valortotal = 0;
+    float salario = 545.0;
+    float comissao;
+    int maisvendido = 0;
+
+    // Entrada de dados
+    for (int i = 0; i < 10; i++) {
+        printf("\nDigite o valor unitário da peça %d: ", i);
+        scanf("%f", &vetorprecovenda[i]);
+        printf("Digite a quantidade vendida da peça %d: ", i);
+        scanf("%d", &vetoruquantidadevendida[i]);
     }
 
     for (int i  = 0; i < 10; i++) {
         valortotal += vetoruquantidadevendida[i] * vetorprecovenda[i];
+    // Relatório
+    printf("\n========== RELATÓRIO ==========\n");
+    printf("Peca | Quantidade | Valor Unitario | Valor Total\n");
+    for (int i = 0; i < 10; i++) {
+        float valtotalpeca = vetorprecovenda[i] * vetoruquantidadevendida[i];
+        printf("%4d | %10d | %13.2f | %11.2f\n", i, vetoruquantidadevendida[i], vetorprecovenda[i], valtotalpeca);
+        valortotal += valtotalpeca;
+        // Verifica o mais vendido
+        if (vetoruquantidadevendida[i] > vetoruquantidadevendida[maisvendido]) {
+            maisvendido = i;
+        }
     }
     printf(" \n =================== R E L A T O R I O =================== \n ");
     printf(" O valor total vendido foi de: %.2lf R$ ", valortotal);
@@ -52,7 +64,14 @@ int main() {
     // fazer a parte do valor unitario das peças e o resto
 
 
+    comissao = valortotal * 0.05;
+    printf("\nValor total das vendas: R$ %.2f", valortotal);
+    printf("\nComissão do vendedor: R$ %.2f", comissao);
+    printf("\nSalário total do vendedor: R$ %.2f", salario + comissao);
 
+    printf("\n\nObjeto mais vendido: %d", maisvendido);
+    printf("\nQuantidade vendida do objeto mais vendido: %d\n", vetoruquantidadevendida[maisvendido]);
 
     return 0;
+}
 }
