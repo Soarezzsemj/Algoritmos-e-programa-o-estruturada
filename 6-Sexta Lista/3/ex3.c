07cc6ff055652941c4c8fc08f13729bbdcee0b79
Soarezzsemj <popcornor6topster@outlook.com>
2025-10-31 23:02:35 -0300

    ex3 lista 6

--- 6-Sexta Lista/3/ex3.c
+++ 6-Sexta Lista/3/ex3.c
@@ -1,6 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
+struct Data {
+    int dia;
+    int mes;
+    int ano;
+};
 
+typedef struct Data date;
 
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    date Data = {0,0,0};
+
+    printf("digite o dia: ");
+    scanf("%d", &Data.dia);
+    fflush(stdin);
+    printf("digite mês: ");
+    scanf("%d", &Data.mes);
+    fflush(stdin);
+    printf("digite o ano: ");
+    scanf("%d", &Data.ano);
+    fflush(stdin);
+
+    printf("%d / %.2d / %d ", Data.dia, Data.mes, Data.ano);
+
+    return 0;
+}
 
 
 //  Struct com data
