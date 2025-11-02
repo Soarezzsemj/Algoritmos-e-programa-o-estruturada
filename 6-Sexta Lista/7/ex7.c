06e3507c1318a7e507fa67e8683b45cfed253064
Soarezzsemj <popcornor6topster@outlook.com>
2025-11-02 00:12:31 -0300

    .

--- 6-Sexta Lista/7/ex7.c
+++ 6-Sexta Lista/7/ex7.c
@@ -3,3 +3,34 @@
 // ponteiros.
 // No main(), teste com dois inteiros.
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void troca(int *x, int*y) {
+
+    int temporaria = *x;
+    *x = *y;
+    *y = temporaria;
+}
+
+int main() {
+
+
+    int x,y;
+
+    printf("Insira o valor de X: ");
+    scanf("%d",&x);
+    printf("Insira o valor de Y: ");
+    scanf("%d",&y);
+
+    troca(&x, &y);
+
+    printf("\nApós a troca:\n");
+    printf("X = %d\n", x);
+    printf("Y = %d\n", y);
+
+
+
+
+    return 0;
+}
--- 6-Sexta Lista/8/ex8.c
+++ 6-Sexta Lista/8/ex8.c
@@ -3,3 +3,28 @@
 // *b.
 // No main(), leia dois números, chame a função e exiba o resultado.
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void somar(int *a, int *b, int *resultado) {
+    *resultado = *a + *b;
+}
+
+int main() {
+
+    int a, b;
+    int resultado;
+
+    printf("digite o valor de A:");
+    scanf("%d", &a);
+    printf("digite o valor de B:");
+    scanf("%d", &b);
+
+    somar(&a, &b, &resultado);
+
+
+    printf("O resultado da soma: %d\n", resultado);
+
+
+    return 0;
+}
