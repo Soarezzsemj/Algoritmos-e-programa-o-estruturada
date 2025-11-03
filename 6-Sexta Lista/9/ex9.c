d509db147d45dc1c70f0355135e25c7cd67902af
Soarezzsemj <popcornor6topster@outlook.com>
2025-11-03 17:09:27 -0300

    ex9 lista 6 feito

--- 6-Sexta Lista/9/ex9.c
+++ 6-Sexta Lista/9/ex9.c
@@ -2,3 +2,23 @@
 // Declare um vetor de 5 números inteiros.
 // Use apenas ponteiros para percorrer e imprimir todos os elementos do vetor (sem usar colchetes
 // []).
+
+#include <stdio.h>
+
+int main() {
+    int numero [5];
+    int *ptr;
+    ptr = &numero[0];
+
+    for (int i = 0; i < 5; i++) {
+        printf("digite o numero[%d]:", i + 1);
+        scanf("%d", ptr + i);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("Numero %d, Casa -> %d\n", *(ptr + i), i+1);
+    }
+
+
+    return 0;
+}
